0900d0f7a8f841091da30365eb5c56f2031e0468
Matt <mcrolen@icloud.com>
2021-02-02 01:10:39 -0500

    Added API to horoscope

--- simple-horoscope/script.js
+++ simple-horoscope/script.js
@@ -1,66 +1,82 @@
 const monthSelector = document.querySelector('#month');
 const daySelector = document.querySelector('#day');
 const result = document.querySelector('#resultField');
+const horoscope = document.querySelector('#horoscope');
+const API_URL = 'http://horoscope-api.herokuapp.com/horoscope/'
 
-monthSelector.addEventListener('change', function() {
+monthSelector.addEventListener('change', function () {
   setMonth(this.value);
 });
 
-daySelector.addEventListener('change', function() {
+daySelector.addEventListener('change', function () {
   setDay(this.value, monthSelector.value);
 });
 
 
 function setMonth(month) {
-  console.log(month);
+  daySelector.disabled = false;
   if (month === 'default') {
     daySelector.innerHTML = '<option value="default" selected>Day</option>';
-  }else if (month === 'apr' || month === 'jun' || month === 'sep' || month === 'nov') {
+    daySelector.disabled = true;
+  } else if (month === 'apr' || month === 'jun' || month === 'sep' || month === 'nov') {
     makeDays(30);
-  } else if (month ==='feb') {
+  } else if (month === 'feb') {
     makeDays(29);
   } else {
     makeDays(31);
   }
-  daySelector.disabled = (month === 'default' ? true : false);
 }
 
 function makeDays(days) {
   daySelector.innerHTML = '<option value="default" selected>Day</option>';
   for (let i = 1; i <= days; i++) {
-  daySelector.innerHTML += `<option value=${i}>${i}</option>`;
+    daySelector.innerHTML += `<option value=${i}>${i}</option>`;
   }
 }
 
 function setDay(day, month) {
-  console.log(month + ' ' + day);
-  if (day) {
+  let sunSign = '';
+  if (day !== 'default') {
     if ((month === 'mar' && day > 20) || (month === 'apr' && day < 20)) {
-      result.innerText = 'ARIES';
+      sunSign = 'aries';
     } else if ((month === 'apr' && day > 19) || (month === 'may' && day < 21)) {
-      result.innerText = 'TAURUS';
+      sunSign = 'taurus';
     } else if ((month === 'may' && day > 20) || (month === 'jun' && day < 21)) {
-      result.innerText = 'GEMINI';
+      sunSign = 'gemini';
     } else if ((month === 'jun' && day > 20) || (month === 'jul' && day < 23)) {
-      result.innerText = 'CANCER';
+      sunSign = 'cancer';
     } else if ((month === 'jul' && day > 22) || (month === 'aug' && day < 23)) {
-      result.innerText = 'LEO';
+      sunSign = 'leo';
     } else if ((month === 'aug' && day > 22) || (month === 'sep' && day < 23)) {
-      result.innerText = 'VIRGO';
+      sunSign = 'virgo';
     } else if ((month === 'sep' && day > 22) || (month === 'oct' && day < 23)) {
-      result.innerText = 'LIBRA';
+      sunSign = 'libra';
     } else if ((month === 'oct' && day > 22) || (month === 'nov' && day < 23)) {
-      result.innerText = 'SCORPIO';
+      sunSign = 'scorpio';
     } else if ((month === 'nov' && day > 22) || (month === 'dec' && day < 22)) {
-      result.innerText = 'SAGITTARIUS';
+      sunSign = 'sagittarius';
     } else if ((month === 'dec' && day > 21) || (month === 'jan' && day < 20)) {
-      result.innerText = 'CAPRICORN';
+      sunSign = 'capricorn';
     } else if ((month === 'jan' && day > 19) || (month === 'feb' && day < 19)) {
-      result.innerText = 'AQUARIUS';
+      sunSign = 'aquarius';
     } else if ((month === 'feb' && day > 18) || (month === 'mar' && day < 21)) {
-      result.innerText = 'PISCES';
+      sunSign = 'pisces';
     } else {
-      result.innerText = 'YOU DONE FUCKED UP';
+      alert('ERROR')
     }
+    getHoroscope(sunSign);
   }
+}
+
+function getHoroscope(sign) {
+  result.innerText = sign.toUpperCase();
+  fetch(API_URL + 'today/' + sign)
+    .then(res => res.json())
+    .then(data => {
+      result.innerText = data.sunsign.toUpperCase();
+      horoscope.innerText = data.horoscope;
+    })
+    .catch(err => {
+      console.log(`error ${err}`);
+  });
 }
